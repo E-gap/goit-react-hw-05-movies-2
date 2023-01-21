46686b02d6cb252849e00103588b9df40f22b561
E-gap <106596181+E-gap@users.noreply.github.com>
2023-01-21 16:17:16 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -65,7 +65,6 @@ export const App = () => {
     <div
       style={{
         height: '100vh',
-        display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
