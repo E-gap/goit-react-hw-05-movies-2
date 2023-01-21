d227f779f3f0058c4378c26d241f445e12c926c8
E-gap <106596181+E-gap@users.noreply.github.com>
2023-01-21 16:15:47 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, lazy } from 'react';
+/* import { useState, useEffect, lazy } from 'react';
 import Home from '../pages/Home/Home';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
@@ -47,15 +47,32 @@ export const App = () => {
     >
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* <Route index element={<Home movies={movies} />}></Route>
+          <Route index element={<Home movies={movies} />}></Route>
           <Route path="movies" element={<Movies />}></Route>
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />}></Route>
             <Route path="reviews" element={<Reviews />}></Route>
           </Route>
         </Route>
-        <Route path="*" element={<Home movies={movies} />} /> */}
+        <Route path="*" element={<Home movies={movies} />} />
       </Routes>
+    </div>
+  );
+}; */
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
     </div>
   );
 };
