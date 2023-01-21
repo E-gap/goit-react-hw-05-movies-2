bf27d2ee205b22e67bf9ec9219ccdf6759755788
E-gap <106596181+E-gap@users.noreply.github.com>
2023-01-21 11:23:03 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import { useState, useEffect, lazy } from 'react';
+import Home from '../pages/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Movies from '../pages/Movies/Movies';
+import { key } from '../services/data';
+
 export const App = () => {
   return (
     <div
